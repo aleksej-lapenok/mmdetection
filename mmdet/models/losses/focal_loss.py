f7a72387957bcf4b80c855e2942db6708db73684
Aleksej Lapenok <live-penguin@yandex-team.ru>
2021-04-18 18:09:09 +0300

    fix focal loss

--- mmdet/models/losses/focal_loss.py
+++ mmdet/models/losses/focal_loss.py
@@ -113,7 +113,7 @@ def sigmoid_focal_loss(focal_s,
     """
     # Function.apply does not accept keyword arguments, so the decorator
     # "weighted_loss" is not applicable
-    loss = _sigmoid_focal_loss(focal_s, pred.contiguous(), target, alpha, gamma, 'none')
+    loss = _sigmoid_focal_loss(focal_s, pred, target, alpha, gamma, 'none')
     if weight is not None:
         if weight.shape != loss.shape:
             if weight.size(0) == loss.size(0):
